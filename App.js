8c938a3b22f5fec776afa83eaa7990fd918dab00
Darionfman <darionfman@gmail.com>
2017-07-01 19:42:34 -0500

    Adds react-native-sound

--- App.js
+++ App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
-
+import { Sound } from 'react-native-sound';
 export default class App extends React.Component {
 
   constructor(props) {
     super(props)
   }
-
+  
   render() {
     return (
       <View style={styles.container}>
         <Text>Darion Freeman is a fucking legend</Text>
         <Image
-           style={{width: 200, height: 200}} 
+           style={{width: 200, height: 200}}
           source={{uri: 'https://ih1.redbubble.net/image.242491557.6081/pp,550x550.jpg'}} />
       </View>
     );
