4dad9f92ef7e49ee9c50aa7b2fceee4ea4961bbf
Darionfman <darionfman@gmail.com>
2017-07-01 19:28:02 -0500

    Ahego support

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Darion Freeman is a fucking legend</Text>
+        <Image
+           style={{width: 200, height: 200}} 
+          source={{uri: 'https://ih1.redbubble.net/image.242491557.6081/pp,550x550.jpg'}} />
       </View>
     );
   }
