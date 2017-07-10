eaec983ac7058fbc97e5df26bf9d2944daac5c6f
Darionfman <darionfman@gmail.com>
2017-07-10 12:51:32 -0500

    Fixes sound issues for ios

--- index.ios.js
+++ index.ios.js
@@ -10,17 +10,45 @@ import {
   StyleSheet,
   Text,
   View,
+  TouchableHighlight,
   Image
 } from 'react-native';
+const Sound = require('react-native-sound');
 
 export default class ReadyUp extends Component {
+
+  constructor(props) {
+    super(props)
+  }
+
+  _playSound() {
+    const whoosh = new Sound('WHAT-THE-FUCK-IS-WRONG-WITH-YOU(mp3cut.net).mp3', Sound.MAIN_BUNDLE, (error) => {
+      if (error) {
+        console.log('failed to load the sound', error);
+        return;
+      }
+      // loaded successfully
+      console.log('duration in seconds: ' + whoosh.getDuration() + 'number of channels: ' + whoosh.getNumberOfChannels());
+      whoosh.play((success) => {
+        if (success) {
+          console.log('successfully finished playing');
+        } else {
+          console.log('playback failed due to audio decoding errors');
+        }
+      });
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Darion Freeman is a fucking legend</Text>
+        <TouchableHighlight
+          onPress={this._playSound}>
         <Image
-           style={{width: 200, height: 200}}
+          style={{width: 200, height: 200}}
           source={{uri: 'https://ih1.redbubble.net/image.242491557.6081/pp,550x550.jpg'}} />
+        </TouchableHighlight>
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
